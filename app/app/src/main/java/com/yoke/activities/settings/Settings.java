49ed2778f68cd28ad583482dd0c20a5c22da6079
kstrahilova <kstrahilova@gmail.com>
2019-03-24 22:02:45 +0100

    preference screen now in activity_settings.xml

--- app/app/src/main/java/com/yoke/activities/settings/Settings.java
+++ app/app/src/main/java/com/yoke/activities/settings/Settings.java
@@ -11,6 +11,7 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.settings_container, new SettingsFragment())
