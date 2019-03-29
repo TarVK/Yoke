aed8ece7dd4b6e7659bc89be045252dde3836ba9
kstrahilova <kstrahilova@gmail.com>
2019-03-29 16:05:38 +0100

    finally managed to fix issues caused by dependency

--- app/app/src/main/java/com/yoke/activities/settings/SettingsFragment.java
+++ app/app/src/main/java/com/yoke/activities/settings/SettingsFragment.java
@@ -1,7 +1,7 @@
 package com.yoke.activities.settings;
 
 import android.content.Intent;
-import android.support.v4.view.LayoutInflaterCompat;
+//import android.support.v4.view.LayoutInflaterCompat;
 import android.view.View;
 import android.view.LayoutInflater;
 import android.os.Bundle;
