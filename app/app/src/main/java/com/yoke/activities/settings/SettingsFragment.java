14f9ffb514fd9bbb90980d0c25b57e3b1c4c66bb
kstrahilova <kstrahilova@gmail.com>
2019-04-12 20:47:24 +0200

    started colour picker implementation, doesnt work yet

--- app/app/src/main/java/com/yoke/activities/settings/SettingsFragment.java
+++ app/app/src/main/java/com/yoke/activities/settings/SettingsFragment.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceManager;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
@@ -15,14 +18,15 @@ import com.yoke.Helper.MainApp;
 import com.yoke.activities.home.HomeActivity;
 import com.yoke.activities.tutorial.TutorialActivity;
 
+import yuku.ambilwarna.AmbilWarnaDialog;
+
 import static com.yoke.activities.splash.GlobalMessageReceiver.getActivity;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
-    //public static String preferences;
     public static SharedPreferences preferences;
-//    LocaleManager localeManager;
-
+    TypedArray colorTypedArray;
+    AmbilWarnaDialog colorPicker;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,12 +36,13 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         Resources res = getContext().getResources();
         android.content.res.Configuration conf = res.getConfiguration();
         //addPreferencesFromResource(R.xml.preferences);
+        //ListPreference colorPicker = new ColorPickerDialog(getContext());
     }
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.preferences);
-        //System.out.println("whatevs");
+
         if (getActivity().getSharedPreferences("preferences", Context.MODE_PRIVATE) == null) {
             Log.e("preferences empty", "true");
             preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
@@ -52,6 +57,8 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                     startActivity(new Intent((getActivity()), TutorialActivity.class));
                 } else if (preference.getKey() == "about") {
                     //github README.txt
+                } else if (preference.getKey() == "color") {
+                    Log.e("clicked", "so far so good");
                 }
 
                 return true;
@@ -62,7 +69,8 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         Preference language = findPreference("language");
 
         if (!preferences.contains("language")) {
-            editor.putString(getString(R.string.language), "en");
+            //the following line was changed since i last checked that language change works
+            editor.putString("language", "en");
             editor.apply();
             setLanguageSummary(language, "en");
         }
@@ -82,7 +90,43 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         });
 
 
+        //Colour Preference
 
+        int colorPrimary = Color.parseColor("@color/colorPrimary");
+        if (!preferences.contains("color")) {
+            editor.putInt("color", colorPrimary);
+            editor.apply();
+        }
+
+        colorPicker = new AmbilWarnaDialog(getContext(), colorPrimary, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                editor.putInt("color", color);
+                editor.apply();
+                Log.e("new color applied", "color is " + color);
+            }
+
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+                // cancel was selected by the user
+            }
+        });
+
+        colorPicker.show();
+        //Preference color = findPreference("color");
+        //colorTypedArray = getContext().getResources().obtainTypedArray(R.array.pref_color_entries);
+
+
+        /*color.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object o) {
+                editor.putString("color", o.toString());
+                editor.commit();
+                setNewColor(o.toString());
+
+                return true;
+            }
+        });*/
 
         //Connection preference
         Preference connection = findPreference("connection");
