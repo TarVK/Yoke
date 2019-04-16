c194cf46bdc4df3f34ba558de5a632ff56c48ea4
Zenden <i.zenden@student.tue.nl>
2019-04-16 19:23:18 +0200

    Update gamer preset

--- app/app/src/main/java/com/yoke/preset/types/GamerPreset.java
+++ app/app/src/main/java/com/yoke/preset/types/GamerPreset.java
@@ -23,10 +23,10 @@ public class GamerPreset extends Preset {
         profile.setName("Gamer");
 
         addMacro("Steam", R.drawable.macro_steam, new OpenProgramCmd("C:\\Program Files (x86)\\Steam\\Steam.exe"));
-        addMacro("Discord", R.drawable.macro_discord, new OpenProgramCmd("C:\\Program Files (x86)\\Discord\\discord.exe"));
+        addMacro("Discord", R.drawable.macro_discord, new OpenURLCmd("https://www.discord.com/"));
         addMacro("Origin", R.drawable.macro_origin, new OpenProgramCmd("C:\\Program Files (x86)\\Origin\\Origin.exe"));
-        addMacro("Uplay", R.drawable.macro_uplay, new OpenProgramCmd("C:\\Program Files (x86)\\Uplay\\Uplay.exe"));
-        addMacro("BattleNet", R.drawable.macro_battlenet,new OpenProgramCmd("C:\\Program Files (x86)\\BattleNey\\Battlenet.exe"));
+        addMacro("Uplay", R.drawable.macro_uplay, new OpenProgramCmd("C:\\Program Files (x86)\\Ubisoft\\Ubisoft Game Launcher\\Uplay.exe"));
+        addMacro("BattleNet", R.drawable.macro_battlenet,new OpenProgramCmd("C:\\Program Files (x86)\\Battle.net\\Battle.net.exe"));
         addMacro("GOG", R.drawable.macro_gog, new OpenURLCmd("https://www.gog.com/"));
     }
 }
